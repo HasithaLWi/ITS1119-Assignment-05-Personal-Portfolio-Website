1c7ce3c00be35b010c7b38d389cbad63a1625f81
Hasitha <hasithawijesinghe2020@gmail.com>
2026-05-03 16:46:19 +0530

    add CV, implement navigation helper, and populate assignment project details , send email

--- script.js
+++ script.js
@@ -35,3 +35,80 @@ document.addEventListener('DOMContentLoaded', () => {
 
 console.log('Script loaded successfully');
 console.log('All sections:', document.querySelectorAll('.visited-section'));
+
+
+// how to navigate another webside in new tab
+
+// 1. using href
+// 2. using window.open
+// 3. using window.location
+
+function openNewTab(url) {
+  window.open(url, '_blank');
+}
+
+// i want to open pdf file to download in current tab not new tab
+//no i want full screen view of pdf file in current tab and i want to download pdf file when click
+
+// for download the file 
+// download pdf file function in current tab 
+
+
+// for open the file in new tab
+
+// open pdf file function in new tab
+function openCVInNewTab() {
+  window.open('assets/Hasitha Wijesinghe CV-26-01.pdf', '_blank');
+  // document.title = "Hasitha Wijesinghe CV";
+}
+
+function validateContactForm() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  if (name === '' || email === '' || message === '') {
+    alert('Please fill all the fields');
+    return false;
+  } else if (name.length < 3) {
+    alert('Name must be at least 3 characters long');
+    return false;
+  } else if (!email.includes('@')) {
+    alert('Invalid email');
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateEmail(email) {
+  const emailRegex = /[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function sendEmail() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+  if (validateContactForm()) {
+    const serviceID = "....";
+    const templateID = "template_tomvhrs";
+
+    emailjs.send(serviceID, templateID, {
+      from_name: name,
+      from_email: email,
+      to_name: "Hasitha",
+      message: message,
+      title: name + " from " + email,
+    })
+      .then(() => {
+        alert("Email sent successfully!");
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+  }
+}
+
+
+
+
